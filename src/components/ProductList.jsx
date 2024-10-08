ef08f094b214161963583c8cbfee02b7b89abeab
Jamesxx98 <jamesmaina445@gmail.com>
2024-10-08 12:10:55 +0300

    details update

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProductCard from './ProductCard';
 import PropTypes from 'prop-types';
 
+
 const ProductList = ({ products }) => {
   if (!products || products.length === 0) {
     return <p>No products available.</p>;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ProductList from '../components/ProductList';
+import './styles/LandingPage.css'; // Import your custom styles
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -7,10 +8,7 @@ const Home = () => {
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
       .then(res => res.json())
-      .then(data => {
-        console.log('Fetched products:', data); // Add this line to log the data
-        setProducts(data);
-      })
+      .then(data => setProducts(data))
       .catch(error => console.error('Error fetching products:', error));
   }, []);
 
@@ -20,12 +18,7 @@ const Home = () => {
         <h1>Welcome to Our Online Store</h1>
         <p>Find the best products for your needs</p>
       </div>
-      {/* Check if products exist */}
-      {products.length > 0 ? (
-        <ProductList products={products} />
-      ) : (
-        <p>Loading products...</p>
-      )}
+      <ProductList products={products} />
     </div>
   );
 };
